1b0b603f07848c810940131496517890d3b4d3b2
DESKTOP-GG7EUA5\HoYean Lee <ghdusjang8@gmail.com>
2020-10-02 18:04:05 +0900

    필요없는 줄 삭제

--- app/src/main/java/com/software/engineering/alcohollife/ui/splash/SplashActivity.java
+++ app/src/main/java/com/software/engineering/alcohollife/ui/splash/SplashActivity.java
@@ -25,7 +25,6 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-                LogUtil.d()
             }
         }, 1000);
     }
