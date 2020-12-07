dbabe78d2221f955b1e5a5be8a460eefbf0a24d4
billion109 <billion109@naver.com>
2020-12-07 20:16:16 +0900

    리뷰 작성할 떄 텍스트 받아오는거 처리

--- app/src/main/java/com/software/engineering/alcohollife/ui/review/WriteReview.java
+++ app/src/main/java/com/software/engineering/alcohollife/ui/review/WriteReview.java
@@ -3,14 +3,39 @@ package com.software.engineering.alcohollife.ui.review;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.software.engineering.alcohollife.R;
 
-public class WriteReview extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class WriteReview extends AppCompatActivity {
+    String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+
+        final EditText editText = findViewById(R.id.editTextTextPersonName);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s = editText.getText().toString();
+                review_submit(s);
+
+            }
+        });
+
+    }
+
+    public void review_submit(String s){
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+
     }
 }
