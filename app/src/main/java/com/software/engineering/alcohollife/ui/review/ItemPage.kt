9760270ed3f5ea2b9eac4936375b41f868a4a45b
billion109 <billion109@naver.com>
2020-12-08 07:19:17 +0900

    아이템페이지 툴바로 변경

--- app/src/main/java/com/software/engineering/alcohollife/ui/review/ItemPage.kt
+++ app/src/main/java/com/software/engineering/alcohollife/ui/review/ItemPage.kt
@@ -3,6 +3,9 @@ package com.software.engineering.alcohollife.ui.review
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
@@ -26,6 +29,13 @@ class ItemPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_page)
 
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+        ab.setDisplayHomeAsUpEnabled(true)
+
+
         val call = model.getAlcohol(name)
         call.enqueue(object : Callback<JsonObject> {
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {}
@@ -48,12 +58,23 @@ class ItemPage : AppCompatActivity() {
         })
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id){
+            android.R.id.home ->{
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     fun setData(data : ItemData){
         Glide.with(this)
             .load(data.image)
             .into(imageView2)
 
-        item_name.text = data.name
+        toolbar_title.text = data.name
         item_name2.text = data.name
     }
 
